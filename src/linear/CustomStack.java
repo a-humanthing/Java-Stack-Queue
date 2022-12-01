ae2173620b955ebe0ad6621feb490a40e6f0d1e9
a-humanthing <arunkrishnakt2002@gmail.com>
2022-12-01 17:12:20 +0530

    first commit

---
+++ src/linear/CustomStack.java
@@ -0,0 +1,45 @@
+package linear;
+
+public class CustomStack {
+	protected int[] data;
+	private static final int DEFAULT_SIZE=10;
+	
+	int ptr = -1;
+	public CustomStack() {
+		this(DEFAULT_SIZE);
+	}
+	public CustomStack(int size){
+		this.data = new int[size];
+	}
+	public boolean push(int item) {
+		if(isFull()) {
+			System.out.println("Stack is full");
+			return false;
+		}
+		ptr++;
+		data[ptr]=item;
+		return true;
+	}
+	public int pop() throws Exception {
+		if(isEmpty()) {
+			throw new Exception("Cannot pop from an empty stack!!");
+		}
+//		int removedItem = data[ptr];
+//		ptr--;
+//		return removedItem;
+		return data[ptr--];
+	}
+	public int peek() throws Exception {
+		if(isEmpty()){
+			throw new Exception("Stack is Emptry");
+		}
+		return data[ptr];
+	}
+	public boolean isFull() {
+		return ptr == data.length-1;
+	}
+	
+	private boolean isEmpty() {
+		return ptr==-1;
+	}
+}
---
+++ src/linear/StackMain.java
@@ -0,0 +1,31 @@
+package linear;
+
+public class StackMain {
+	public static void main(String[] args) {
+		DynamicStack stack = new DynamicStack(4);
+		stack.push(4);
+		stack.push(16);
+		stack.push(3);
+		stack.push(31);
+		stack.push(12);
+		stack.push(11);
+		stack.push(16);
+		stack.push(16);
+		
+		try {
+			System.out.println(stack.pop());
+			System.out.println(stack.pop());
+			System.out.println(stack.pop());
+			System.out.println(stack.pop());
+			System.out.println(stack.pop());
+			System.out.println(stack.pop());
+			
+			System.out.println("Peek = "+stack.peek());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.out.println(e.getMessage());
+		}
+
+		
+	}
+}
